a1da282090182c7ea083b47b33a15fae190c8387
Gabriel de Souza Valenço <gabrielsvalenco@gmail.com>
2025-07-08 11:31:51 -0300

    feat: initial project setup
    - Configured Node.js with TypeScript
    - Added Fastify as the web framework
    - Integrated Zod for schema validation
    - Set up basic project structure
    - Added initial configuration files (package.json, tsconfig.json, .env)

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import { fastify } from 'fastify'
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifyCors} from '@fastify/cors'
+
+const app = fastify ()
+
+app.register(fastifyCors, {
+    origin: 'http://localhost:5173',
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.listen({ port: process.env.PORT ? Number(process.env.PORT) : 3333}).then(() => {
+    console.log(`Port: ${process.env.PORT}`)
+    console.log('Server is Okay!!')
+})
